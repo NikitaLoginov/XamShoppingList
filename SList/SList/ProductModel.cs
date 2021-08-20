b043b6bb09aea248ef349be5f80843d637c1889b
NikitaLoginov <56047823+NikitaLoginov@users.noreply.github.com>
2021-08-20 11:20:16 +0300

    naming

---
+++ SList/SList/ProductModel.cs
@@ -0,0 +1,13 @@
+﻿using SQLite;
+
+namespace SList
+{
+    public class ProductModel
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public string Notes { get; set; }
+    }
+}
